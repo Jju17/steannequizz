7aac333ea9a98975c981b2dd1d5697fed5d65ec3
Jju17 <julien.rahier@gmail.com>
2020-06-19 13:13:49 +0200

    modifs

--- src/components/quizz-list/QuizzAnswer.js
+++ src/components/quizz-list/QuizzAnswer.js
@@ -4,7 +4,9 @@ export default class QuizzElement extends Component {
   render() {
     return (
       <div className="container d-flex flex-column justify-content-center align-items-center">
-        <h1 className="mb-5">Tu es un... {this.props.name} </h1>
+        <h1 className="mb-5">
+          Félicitations! Tu es le {this.props.name} de Ste Anne!
+        </h1>
         <p className="lead text-muted">{this.props.desc}</p>
       </div>
     );
--- src/dataA.js
+++ src/dataA.js
@@ -1,42 +1,42 @@
 export default [
   {
-    name: "Percy",
+    name: "prédateur",
     desc:
       "Après tout, c’est grâce à des crasses comme toi que la flamme du scandale de Ste-Anne reste encore un peu allumée.",
   },
   {
-    name: "Azara/Erwan",
+    name: "beauf",
     desc:
       "L’alcool fait ressortir de toi ce que la décence t’obligeait à cacher.",
   },
   {
-    name: "Baraz",
+    name: "arrogant",
     desc: "",
   },
   {
-    name: "Arsh/De Drée",
+    name: "nudiste",
     desc:
       "Vêtements c’est surfait, que ce soit parce que t’es fier de ton corps de bodybuilder ou qu’il y a un affond brouillard qui se perd, tu perd tes vêtements en teuf plus vite que les chefs d’u perdent l’attention des gens en CU",
   },
   {
-    name: "Griz",
+    name: "inaudible",
     desc: "Vois le côté positif des choses",
   },
   {
-    name: "Tay",
+    name: "aggressif",
     desc:
       "Que ce soit à cause de ton allergie au gluten ou parce que tes animés t’ont fait chier en journée, t’es pas là pour t’amuser en soirée, mais pour briser des bonnes humeurs dans la plus grande gratuité qui soit. Après tout, facile de t’excuser après en disant que c’est la faut de la bière.",
   },
   {
-    name: "Simon",
+    name: "danceur",
     desc: "Les légendes racontent que t’as passé plus",
   },
   {
-    name: "Aari",
+    name: "sociable",
     desc: "",
   },
   {
-    name: "Tanguy/Margay",
+    name: "batman",
     desc:
       "Si les gens devaient choisir entre observer une éclipse solaire et te croiser en soirée, leur choix serait vite fait, ",
   },
